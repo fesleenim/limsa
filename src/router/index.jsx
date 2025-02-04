0fabfec8fb0681962c897053f9389747e9fb6ae1
Fesleen <mosajonovae@gmail.com>
2025-02-04 12:28:23 +0500

    lalala

--- src/router/index.jsx
+++ src/router/index.jsx
@@ -11,7 +11,7 @@ export const Router = createBrowserRouter([
         element: <HomePages />,
     },
     {
-        path: "/",
+        path: "/App",
         element: <App />,
         children:[
             {
